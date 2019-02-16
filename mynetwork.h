a9a6c510d7270b537f12cca3ec41e2f792600dac
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2019-02-16 12:18:19 +0100

    multiple Thread compliant

--- mynetwork.h
+++ mynetwork.h
@@ -2,6 +2,7 @@
 #define MYNETWORK_H
 
 #include <QNetworkAccessManager>
+#include <QThread>
 
 class MyNetwork
 {
@@ -11,8 +12,16 @@ public:
 
     static QNetworkAccessManager &manager()
     {
-        static QNetworkAccessManager manager;
-        return manager;
+        static QHash<QString, QNetworkAccessManager*> thread_manager;
+
+        QString current_thread_pointer = QString().sprintf("%8p", static_cast<void*>(QThread::currentThread()));
+
+        if (thread_manager.contains(current_thread_pointer))
+            return *thread_manager[current_thread_pointer];
+
+        auto manager = new QNetworkAccessManager();
+        thread_manager[current_thread_pointer] = manager;
+        return *manager;
     }
 
 private:
